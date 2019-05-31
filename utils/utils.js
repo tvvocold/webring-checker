3da0a8e1d1460c5faa13357ec847446250ece545
Ray <tvvocold@gmail.com>
2019-05-31 23:59:14 +0800

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -12,7 +12,7 @@ const cleanLine = line => {
 
 const gatherSiteObjects = () => {
   return new Promise((resolve, reject) => {
-    request('https://webring.xxiivv.com/scripts/sites.js', (err, _, body) => {
+    request('https://webring.captainray.blog/scripts/sites.js', (err, _, body) => {
       if (!err) {
         const begin = body.indexOf('[') + 1
         const end = body.indexOf(']')
